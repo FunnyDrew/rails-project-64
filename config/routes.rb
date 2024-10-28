d77781118aefd9006deb104307c3d04eeca48e0b
FunnyDrew <rybin.ag@gmail.com>
2024-10-28 10:28:18 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,26 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your applicaStion routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
 
   root "posts#index"
 
-  resources :posts,only: [:new, :create, :show] do
+  resources :posts, only: [:new, :create, :show] do
     scope module: :posts do
-      resources :comments, shallow: true
-      resources :likes, shallow: true
+      resources :comments
+      resources :likes, only: :create
+      resources :likes, only: :destroy      
     end
   end
+
+  
+
+
+  
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
