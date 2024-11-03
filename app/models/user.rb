930979f54f5be385dcd4193b4b7d6a8a0713357b
FunnyDrew <rybin.ag@gmail.com>
2024-11-03 12:32:25 +0300

    fix some slim-lint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'creator_id', dependent: :destroy
+  has_many :posts, foreign_key: 'creator_id', dependent: :destroy, inverse_of: :users
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   # Include default devise modules. Others available are:
