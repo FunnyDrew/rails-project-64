6a72f5caa3729ef33825731088aed92d012f0d30
FunnyDrew <rybin.ag@gmail.com>
2024-11-06 07:15:42 +0300

    add test of comment

--- test/controllers/posts/comments_controller_test.rb
+++ test/controllers/posts/comments_controller_test.rb
@@ -3,15 +3,28 @@
 require 'test_helper'
 
 class Posts::CommentsControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
+  include Devise::Test::IntegrationHelpers 
+
+  setup do
+    @user = users(:one)
+    @post = posts(:post_without_comment)
+    @attrs = {
+      content: Faker::Lorem.paragraph(sentence_count: 2),
+      category: categories(:one)
+    }
+    sign_in @user
+  end
   
   test 'create comment by registred user' do
-    skip
-    @user = users(:one)
-    @post = posts(:unliked_post)
-    sign_in @user
-    assert_difference("@post.likes.count", 1) do
-      post post_likes_url @post
+    assert_difference("@post.comments.count", 1) do
+      #debugger
+      post post_comments_url  @post, params: {post_comment: @attrs}
     end
+    #assert{true}
   end
+
+  test 'create subcomment' do
+    
+  end
+
 end
