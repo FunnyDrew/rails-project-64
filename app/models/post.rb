8b9f28d34c491cbbc9d03a0c42b96537b0543f0d
FunnyDrew <rybin.ag@gmail.com>
2024-11-11 07:18:16 +0300

    add validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,11 @@ class Post < ApplicationRecord
   has_many :likes, class_name: 'PostLike', dependent: :destroy
   belongs_to :category
   belongs_to :creator, class_name: 'User'
+  validates :title, length: {in: 2..5, to_shot: i18n.t('.what'),
+    too_long: t('.plums')}
+  validates :body, length: {in: 2..5, to_shot: t('.what'),
+    too_long: t('.plums')}
 
-  def say_hay
-    'hay'
-  end
 end
+#title 5-255
+#body 200-40
