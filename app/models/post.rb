03ee3c130132003da337e8e3ad10cbf108991908
FunnyDrew <rybin.ag@gmail.com>
2024-11-15 07:16:40 +0300

    add cut post function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  MAX_LENGTH_ON_INDEX_PAGE = 200
+
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
   belongs_to :category
   belongs_to :creator, class_name: 'User'
   validates :title, length: { in: 5..255 }
   validates :body, length: { in: 100..1000 }
+
+  def cut
+    "#{self.body.slice(0..MAX_LENGTH_ON_INDEX_PAGE)}..."
+  end
 end
-# title 5-255
-# body 200-40
+
