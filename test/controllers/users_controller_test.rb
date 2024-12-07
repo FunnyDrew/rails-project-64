594957f6af1d84ecc5522e2a3ec935f4bc4bf001
FunnyDrew <rybin.ag@gmail.com>
2024-12-07 18:20:34 +0300

    add tests for users_controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,16 +3,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   test 'show statistic page to owner' do
-
-  end
-
-  test 'redirect if statistic page try open not auntificated user' do
-    
+    @user = users(:one)
+    sign_in @user
+    get user_path(@user.id)
+    assert_response(:success)
   end
 
   test 'redirect if statistic page try open not owner' do
-    
+    @user_watcher = users(:one)
+    @user_owner = users(:two)
+    sign_in @user_watcher
+    get user_path(@user_owner.id)
+    assert_response(:redirect)
   end
 
 end
