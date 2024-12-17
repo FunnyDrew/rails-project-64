1ce20f68fa8ce34a9a7c7f63e8efaf5b01ad9a46
FunnyDrew <rybin.ag@gmail.com>
2024-12-17 07:19:01 +0300

    error with likes counter

--- db/migrate/20241216075130_reset_all_post_cache_counters.rb
+++ db/migrate/20241216075130_reset_all_post_cache_counters.rb
@@ -1,4 +1,18 @@
 class ResetAllPostCacheCounters < ActiveRecord::Migration[7.1]
-  def change
+
+  def up
+
+    Post.all.each do |post|
+
+      Post.reset_counters(post.id, :likes)
+
+    end
+
+  end
+
+  def down
+
+     # no rollback needed
+
   end
 end
